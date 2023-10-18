8ec1f54cf10b0afeb87f9d7c6cb6118a254856d9
Anna Litovskaya <alitovskaya@amplify.com>
2023-10-18 10:15:32 -0400

    simple stage with bunnies

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,36 @@
 /* eslint-disable react/prefer-stateless-function */
 
 import React, { Component } from 'react';
+import { BlurFilter } from 'pixi.js';
+import { Stage, Container, Sprite, Text } from '@pixi/react';
 
 class App extends Component {
   render() {
-    return <div>Hiya</div>;
+    // eslint-disable-next-line react/react-in-jsx-scope
+    return (
+      <Stage width={300} height={300} options={{ backgroundColor: 0xeef1f5 }}>
+        <Container position={[150, 150]}>
+          <Sprite
+            anchor={0.5}
+            x={-75}
+            y={-75}
+            image="https://pixijs.io/pixi-react/img/bunny.png"
+          />
+          <Sprite
+            anchor={0.5}
+            x={0}
+            y={0}
+            image="https://pixijs.io/pixi-react/img/bunny.png"
+          />
+          <Sprite
+            anchor={0.5}
+            x={75}
+            y={75}
+            image="https://pixijs.io/pixi-react/img/bunny.png"
+          />
+        </Container>
+      </Stage>
+    );
   }
 }
 
